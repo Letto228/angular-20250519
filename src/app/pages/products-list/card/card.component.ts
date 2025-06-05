b48558dd715e0e2d4e884e1a334882e189789168
Misha <wrongsky1@yandex.ru>
2025-06-05 09:41:30 +0300

    feat: dz-4 add input\output to card

--- src/app/pages/products-list/card/card.component.ts
+++ src/app/pages/products-list/card/card.component.ts
@@ -1,8 +1,8 @@
-import {ChangeDetectionStrategy, Component, computed, signal} from '@angular/core';
+import {ChangeDetectionStrategy, Component, input, output} from '@angular/core';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
-import {productsMock} from '../../../shared/products/products.mock';
+import {Product, ProductInfo} from '../../../shared/products/product.interface';
 
 @Component({
     selector: 'app-card',
@@ -13,93 +13,32 @@ import {productsMock} from '../../../shared/products/products.mock';
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CardComponent {
-    readonly count = signal(0);
+    readonly product = input<Product>();
+    readonly count = input<number>();
+    readonly addToCartHandler = output<ProductInfo>();
 
-    readonly product = computed(() => productsMock[this.count()]);
-
-    // eslint-disable-next-line max-statements
-    constructor() {
-        setInterval(() => {
-            this.count.update(count => count + 1);
-        }, 1000);
-
-        const showCount = signal(false);
-        const count = signal(0);
-        const conditionalCount = computed(() => {
-            console.warn('Computed calculated');
-
-            return showCount() ? `The count: ${count()}` : `Nothing`;
-        });
-
-        // eslint-disable-next-line no-console
-        console.log(conditionalCount());
-        // eslint-disable-next-line no-console
-        console.log(conditionalCount());
-        // eslint-disable-next-line no-console
-        console.log(conditionalCount());
-
-        // eslint-disable-next-line no-console
-        console.log('Update count');
-        count.update(count => count + 1);
-
-        // eslint-disable-next-line no-console
-        console.log(conditionalCount());
-
-        // eslint-disable-next-line no-console
-        console.log('Update count');
-        count.update(count => count + 1);
-
-        // eslint-disable-next-line no-console
-        console.log(conditionalCount());
-
-        // eslint-disable-next-line no-console
-        console.log('Update count');
-        count.update(count => count + 1);
-
-        // eslint-disable-next-line no-console
-        console.log(conditionalCount());
-
-        // eslint-disable-next-line no-console
-        console.log('Show count: true');
-        showCount.set(true);
-
-        // eslint-disable-next-line no-console
-        console.log(conditionalCount());
-        // eslint-disable-next-line no-console
-        console.log(conditionalCount());
-        // eslint-disable-next-line no-console
-        console.log(conditionalCount());
-
-        // eslint-disable-next-line no-console
-        console.log('Update count');
-        count.update(count => count + 1);
-
-        // eslint-disable-next-line no-console
-        console.log(conditionalCount());
-
-        // eslint-disable-next-line no-console
-        console.log('Update count');
-        count.update(count => count + 1);
-
-        // eslint-disable-next-line no-console
-        console.log(conditionalCount());
-
-        // eslint-disable-next-line no-console
-        console.log('Update count');
-        count.update(count => count + 1);
-
-        // eslint-disable-next-line no-console
-        console.log(conditionalCount());
-    }
-
-    onProductBuy(event: Event) {
+    onBuyButton(event: Event) {
         event.stopPropagation();
-
         // eslint-disable-next-line no-console
         console.log('Buy product');
+        const product = this.product();
+
+        if (product) {
+            const productInfo = {
+                id: product._id,
+                name: product.name,
+                price: product.price,
+                image: product.images[0].url,
+                quantity: 1,
+            };
+
+            this.addToCartHandler.emit(productInfo);
+        }
     }
 
     isStarActive(starIndex: number): boolean {
-        return !!this.product() && this.product().rating >= starIndex;
+        const product = this.product();
+
+        return !!product && product?.rating >= starIndex;
     }
 }
--- src/app/pages/products-list/products-list.component.ts
+++ src/app/pages/products-list/products-list.component.ts
@@ -1,5 +1,7 @@
-import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {ChangeDetectionStrategy, Component, computed, signal} from '@angular/core';
 import {CardComponent} from './card/card.component';
+import {Product, ProductInfo} from '../../shared/products/product.interface';
+import {productsMock} from '../../shared/products/products.mock';
 
 @Component({
     selector: 'app-products-list',
@@ -9,4 +11,20 @@ import {CardComponent} from './card/card.component';
     styleUrl: './products-list.component.css',
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ProductsListComponent {}
+export class ProductsListComponent {
+    readonly count = signal(0);
+    readonly product = computed<Product>(() => productsMock[this.count() % productsMock.length]);
+    readonly cart = signal<ProductInfo[]>([]);
+
+    constructor() {
+        setInterval(() => {
+            this.count.update(c => c + 1);
+        }, 1000);
+    }
+
+    addProductToCart(product: ProductInfo) {
+        this.cart.set([...this.cart(), product]);
+        // eslint-disable-next-line no-console
+        console.log('In cart now:', this.cart());
+    }
+}
