f99c3fba3a2cc7a3ab5c865b868318e48afacb95
Pavel Semenov <pavel.semenov@corp.ktelabs.ru>
2025-06-07 20:36:46 +0300

    feat: homework-lecture-4 add input\output to card

--- src/app/pages/products-list/products-list.component.ts
+++ src/app/pages/products-list/products-list.component.ts
@@ -1,5 +1,7 @@
-import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {ChangeDetectionStrategy, Component, computed, signal} from '@angular/core';
 import {CardComponent} from './card/card.component';
+import {Product} from '../../shared/products/product.interface';
+import {productsMock} from '../../shared/products/products.mock';
 
 @Component({
     selector: 'app-products-list',
@@ -9,4 +11,25 @@ import {CardComponent} from './card/card.component';
     styleUrl: './products-list.component.css',
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ProductsListComponent {}
+export class ProductsListComponent {
+    readonly count = signal(0);
+    readonly product = computed<Product>(() => productsMock[this.count() % productsMock.length]);
+    readonly cart = signal<Product[]>([]);
+
+    constructor() {
+        setInterval(() => {
+            this.count.update(c => c + 1);
+        }, 1000);
+    }
+
+    onBuyProduct(id: string) {
+        const product: Product | undefined = productsMock.find(item => item._id === id);
+
+        if (product && !this.cart().includes(product)) {
+            this.cart.set([...this.cart(), product]);
+        }
+
+        // eslint-disable-next-line no-console
+        console.log('Current cart:', this.cart());
+    }
+}
