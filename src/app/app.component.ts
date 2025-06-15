eb649cf75130acbf47a349dc1848e3cb6c4cdafd
Aleksandr Prokhorenkov <89263379173@yandex.ru>
2025-06-15 09:55:14 +0300

    Lesson-5-homework

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
 import {ChangeDetectionStrategy, Component, signal} from '@angular/core';
+import {CommonModule} from '@angular/common';
 import {HeaderComponent} from './app-components/header/header.component';
 import {ProductsListComponent} from './pages/products-list/products-list.component';
 import {SidenavComponent} from './app-components/sidenav/sidenav.component';
 import {applicationConfigMock} from './shared/application-config/application-config.mock';
+import {PopupHostComponent} from './app-components/popup-host/popup-host.component';
 
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [HeaderComponent, ProductsListComponent, SidenavComponent],
+    imports: [
+        HeaderComponent,
+        ProductsListComponent,
+        SidenavComponent,
+        PopupHostComponent,
+        CommonModule,
+    ],
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
     changeDetection: ChangeDetectionStrategy.OnPush,
