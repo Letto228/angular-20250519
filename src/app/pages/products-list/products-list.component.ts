43e21141646ca0cc51a90618a3f6dc9b870b595f
Letto228 <letto1101@gmail.com>
2025-06-23 22:47:05 +0300

    chore: sanitize project

--- src/app/pages/products-list/products-list.component.ts
+++ src/app/pages/products-list/products-list.component.ts
@@ -1,5 +1,4 @@
-import {ChangeDetectionStrategy, Component, ElementRef, inject, signal} from '@angular/core';
-import {NgFor, NgIf, NgTemplateOutlet} from '@angular/common';
+import {ChangeDetectionStrategy, Component, signal} from '@angular/core';
 import {CardComponent} from './card/card.component';
 import {productsMock} from '../../shared/products/products.mock';
 import {Product} from '../../shared/products/product.interface';
@@ -8,25 +7,17 @@ import {InsertShadowDirective} from '../../shared/insert-shadow/insert-shadow.di
 @Component({
     selector: 'app-products-list',
     standalone: true,
-    imports: [CardComponent, NgFor, NgIf, NgTemplateOutlet, InsertShadowDirective],
+    imports: [CardComponent, InsertShadowDirective],
     templateUrl: './products-list.component.html',
     styleUrl: './products-list.component.css',
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ProductsListComponent {
-    readonly element = inject(ElementRef);
     readonly products = signal<Product[] | null>(null);
 
     constructor() {
-        // eslint-disable-next-line no-console
-        console.log(this.element.nativeElement);
         setTimeout(() => {
             this.products.set(productsMock);
         }, 3000);
-    }
-
-    trackBy(_index: number, item: Product) {
-        // return item; // default
-        return item._id;
     }
 }
