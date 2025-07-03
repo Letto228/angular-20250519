8d8d0829d3f7684d92f8d286de926bb95736ea92
Letto228 <letto1101@gmail.com>
2025-07-03 23:09:05 +0300

    chore: add homework stand

--- src/app/app-components/header/header.component.ts
+++ src/app/app-components/header/header.component.ts
@@ -1,4 +1,4 @@
-import {ChangeDetectionStrategy, Component, input, output} from '@angular/core';
+import {ChangeDetectionStrategy, Component, input, output, TemplateRef} from '@angular/core';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
@@ -15,4 +15,12 @@ import {ApplicationConfig} from '../../shared/application-config/application-con
 export class HeaderComponent {
     readonly appConfig = input.required<ApplicationConfig>();
     readonly menuClick = output<Event>();
+
+    openPopup(_template: TemplateRef<{$implicit: string}>) {
+        // this.popupService.openPopup(template, context);
+    }
+
+    closePopup() {
+        // this.popupService.closePopup();
+    }
 }
